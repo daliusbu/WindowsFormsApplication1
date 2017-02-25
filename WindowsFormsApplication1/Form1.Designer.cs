775da9035180506799e8c1d285f02fcd3b5e52d7
daliusbu <daliusbu@gmail.com>
2017-02-25 19:19:48 +0200

    Kalkuliatorius 2

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Pradzia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.M1 = new System.Windows.Forms.Button();
+            this.M6 = new System.Windows.Forms.Button();
+            this.M5 = new System.Windows.Forms.Button();
+            this.M4 = new System.Windows.Forms.Button();
+            this.M3 = new System.Windows.Forms.Button();
+            this.M2 = new System.Windows.Forms.Button();
+            this.M7 = new System.Windows.Forms.Button();
+            this.M8 = new System.Windows.Forms.Button();
+            this.M9 = new System.Windows.Forms.Button();
+            this.M0 = new System.Windows.Forms.Button();
+            this.Kabl = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Plius = new System.Windows.Forms.Button();
+            this.Kart = new System.Windows.Forms.Button();
+            this.Dalint = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.failasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rasomasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trikampioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.skaiciavimasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.C = new System.Windows.Forms.Button();
+            this.Lygu = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.lygyba = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // M1
+            // 
+            this.M1.Location = new System.Drawing.Point(12, 72);
+            this.M1.Name = "M1";
+            this.M1.Size = new System.Drawing.Size(43, 33);
+            this.M1.TabIndex = 0;
+            this.M1.Text = "1";
+            this.M1.UseVisualStyleBackColor = true;
+            this.M1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M6
+            // 
+            this.M6.Location = new System.Drawing.Point(110, 111);
+            this.M6.Name = "M6";
+            this.M6.Size = new System.Drawing.Size(43, 33);
+            this.M6.TabIndex = 1;
+            this.M6.Text = "6";
+            this.M6.UseVisualStyleBackColor = true;
+            this.M6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M5
+            // 
+            this.M5.Location = new System.Drawing.Point(61, 111);
+            this.M5.Name = "M5";
+            this.M5.Size = new System.Drawing.Size(43, 33);
+            this.M5.TabIndex = 2;
+            this.M5.Text = "5";
+            this.M5.UseVisualStyleBackColor = true;
+            this.M5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M4
+            // 
+            this.M4.Location = new System.Drawing.Point(12, 111);
+            this.M4.Name = "M4";
+            this.M4.Size = new System.Drawing.Size(43, 33);
+            this.M4.TabIndex = 3;
+            this.M4.Text = "4";
+            this.M4.UseVisualStyleBackColor = true;
+            this.M4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M3
+            // 
+            this.M3.Location = new System.Drawing.Point(110, 72);
+            this.M3.Name = "M3";
+            this.M3.Size = new System.Drawing.Size(43, 33);
+            this.M3.TabIndex = 4;
+            this.M3.Text = "3";
+            this.M3.UseVisualStyleBackColor = true;
+            this.M3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M2
+            // 
+            this.M2.Location = new System.Drawing.Point(61, 72);
+            this.M2.Name = "M2";
+            this.M2.Size = new System.Drawing.Size(43, 33);
+            this.M2.TabIndex = 5;
+            this.M2.Text = "2";
+            this.M2.UseVisualStyleBackColor = true;
+            this.M2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M7
+            // 
+            this.M7.Location = new System.Drawing.Point(12, 150);
+            this.M7.Name = "M7";
+            this.M7.Size = new System.Drawing.Size(43, 33);
+            this.M7.TabIndex = 8;
+            this.M7.Text = "7";
+            this.M7.UseVisualStyleBackColor = true;
+            this.M7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M8
+            // 
+            this.M8.Location = new System.Drawing.Point(61, 150);
+            this.M8.Name = "M8";
+            this.M8.Size = new System.Drawing.Size(43, 33);
+            this.M8.TabIndex = 7;
+            this.M8.Text = "8";
+            this.M8.UseVisualStyleBackColor = true;
+            this.M8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M9
+            // 
+            this.M9.Location = new System.Drawing.Point(110, 150);
+            this.M9.Name = "M9";
+            this.M9.Size = new System.Drawing.Size(43, 33);
+            this.M9.TabIndex = 6;
+            this.M9.Text = "9";
+            this.M9.UseVisualStyleBackColor = true;
+            this.M9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // M0
+            // 
+            this.M0.Location = new System.Drawing.Point(13, 189);
+            this.M0.Name = "M0";
+            this.M0.Size = new System.Drawing.Size(91, 33);
+            this.M0.TabIndex = 10;
+            this.M0.Text = "0";
+            this.M0.UseVisualStyleBackColor = true;
+            this.M0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Kabl
+            // 
+            this.Kabl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kabl.Location = new System.Drawing.Point(110, 189);
+            this.Kabl.Name = "Kabl";
+            this.Kabl.Size = new System.Drawing.Size(43, 33);
+            this.Kabl.TabIndex = 9;
+            this.Kabl.Text = ",";
+            this.Kabl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Kabl.UseVisualStyleBackColor = true;
+            this.Kabl.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Minus.Location = new System.Drawing.Point(159, 189);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(43, 33);
+            this.Minus.TabIndex = 15;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // Plius
+            // 
+            this.Plius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Plius.Location = new System.Drawing.Point(159, 150);
+            this.Plius.Name = "Plius";
+            this.Plius.Size = new System.Drawing.Size(43, 33);
+            this.Plius.TabIndex = 14;
+            this.Plius.Text = "+";
+            this.Plius.UseVisualStyleBackColor = true;
+            this.Plius.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // Kart
+            // 
+            this.Kart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kart.Location = new System.Drawing.Point(159, 72);
+            this.Kart.Name = "Kart";
+            this.Kart.Size = new System.Drawing.Size(43, 33);
+            this.Kart.TabIndex = 13;
+            this.Kart.Text = "*";
+            this.Kart.UseVisualStyleBackColor = true;
+            this.Kart.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // Dalint
+            // 
+            this.Dalint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Dalint.Location = new System.Drawing.Point(159, 111);
+            this.Dalint.Name = "Dalint";
+            this.Dalint.Size = new System.Drawing.Size(43, 33);
+            this.Dalint.TabIndex = 12;
+            this.Dalint.Text = "/";
+            this.Dalint.UseVisualStyleBackColor = true;
+            this.Dalint.Click += new System.EventHandler(this.operator_Click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.result.Location = new System.Drawing.Point(13, 34);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(238, 32);
+            this.result.TabIndex = 16;
+            this.result.Text = "0";
+            this.result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.failasToolStripMenuItem,
+            this.rasomasToolStripMenuItem,
+            this.trikampioToolStripMenuItem,
+            this.skaiciavimasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(265, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // failasToolStripMenuItem
+            // 
+            this.failasToolStripMenuItem.Name = "failasToolStripMenuItem";
+            this.failasToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.failasToolStripMenuItem.Text = "Failas";
+            // 
+            // rasomasToolStripMenuItem
+            // 
+            this.rasomasToolStripMenuItem.Name = "rasomasToolStripMenuItem";
+            this.rasomasToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.rasomasToolStripMenuItem.Text = "Rasomas";
+            // 
+            // trikampioToolStripMenuItem
+            // 
+            this.trikampioToolStripMenuItem.Name = "trikampioToolStripMenuItem";
+            this.trikampioToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.trikampioToolStripMenuItem.Text = "Trikampio";
+            // 
+            // skaiciavimasToolStripMenuItem
+            // 
+            this.skaiciavimasToolStripMenuItem.Name = "skaiciavimasToolStripMenuItem";
+            this.skaiciavimasToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.skaiciavimasToolStripMenuItem.Text = "Skaiciavimas";
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.C.Location = new System.Drawing.Point(208, 111);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(43, 33);
+            this.C.TabIndex = 19;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // Lygu
+            // 
+            this.Lygu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Lygu.Location = new System.Drawing.Point(208, 150);
+            this.Lygu.Name = "Lygu";
+            this.Lygu.Size = new System.Drawing.Size(43, 71);
+            this.Lygu.TabIndex = 20;
+            this.Lygu.Text = "=";
+            this.Lygu.UseVisualStyleBackColor = true;
+            this.Lygu.Click += new System.EventHandler(this.Lygu_Click);
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.CE.Location = new System.Drawing.Point(208, 71);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(43, 33);
+            this.CE.TabIndex = 21;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // lygyba
+            // 
+            this.lygyba.AutoSize = true;
+            this.lygyba.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lygyba.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lygyba.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lygyba.Location = new System.Drawing.Point(23, 41);
+            this.lygyba.Name = "lygyba";
+            this.lygyba.Size = new System.Drawing.Size(0, 20);
+            this.lygyba.TabIndex = 22;
+            // 
+            // Pradzia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(265, 248);
+            this.Controls.Add(this.lygyba);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.Lygu);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plius);
+            this.Controls.Add(this.Kart);
+            this.Controls.Add(this.Dalint);
+            this.Controls.Add(this.M0);
+            this.Controls.Add(this.Kabl);
+            this.Controls.Add(this.M7);
+            this.Controls.Add(this.M8);
+            this.Controls.Add(this.M9);
+            this.Controls.Add(this.M2);
+            this.Controls.Add(this.M3);
+            this.Controls.Add(this.M4);
+            this.Controls.Add(this.M5);
+            this.Controls.Add(this.M6);
+            this.Controls.Add(this.M1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Pradzia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkuliatorius";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button M1;
+        private System.Windows.Forms.Button M6;
+        private System.Windows.Forms.Button M5;
+        private System.Windows.Forms.Button M4;
+        private System.Windows.Forms.Button M3;
+        private System.Windows.Forms.Button M2;
+        private System.Windows.Forms.Button M7;
+        private System.Windows.Forms.Button M8;
+        private System.Windows.Forms.Button M9;
+        private System.Windows.Forms.Button M0;
+        private System.Windows.Forms.Button Kabl;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Plius;
+        private System.Windows.Forms.Button Kart;
+        private System.Windows.Forms.Button Dalint;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem failasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rasomasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trikampioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem skaiciavimasToolStripMenuItem;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button Lygu;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Label lygyba;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Pradzia : Form
+    {
+        double value = 0;
+        string oper = "";
+        bool oper_pressed = false;
+
+        public Pradzia()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (oper_pressed))
+                result.Clear();
+            oper_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            value = Double.Parse(result.Text);
+            oper = b.Text;
+            oper_pressed = true;
+            lygyba.Text = value + " " + oper;
+        }
+
+        private void Lygu_Click(object sender, EventArgs e)
+        {
+            lygyba.Text = "=";
+            switch (oper)
+            {
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            oper_pressed = true;
+      }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            result.Clear();
+            value = 0;
+            result.Text = "0";
+        }
+    }
+}
